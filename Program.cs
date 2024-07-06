551d9f5f124fadd550a6352d348fc769d2411b10
elncode <174897089+elncode@users.noreply.github.com>
2024-07-06 19:39:02 +0200

    Proyecto de inicio
    Código inicial obtenido de la web de Twilio con el que poder probar el envío de sms.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Reemplazar con tu Account SID de Twilio
+        var accountSid = "**************************";
+        
+        // Reemplazar con tu Auth Token de Twilio
+        var authToken = "**************************";
+        
+        TwilioClient.Init(accountSid, authToken);
+
+        // Reemplazar con el número de teléfono del destinatario
+        var messageOptions = new CreateMessageOptions(
+            new PhoneNumber("***********"));
+        
+        // Reemplazar con tu número de teléfono de Twilio
+        messageOptions.From = new PhoneNumber("***********");
+        
+        messageOptions.Body = "Hello from Twilio";
+
+        var message = MessageResource.Create(messageOptions);
+        Console.WriteLine(message.Body);
+    }
+}
